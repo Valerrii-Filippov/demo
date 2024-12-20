ae1bed3cb63392a4b62d1caef47edba8b7c7707f
User <filson.mark777@gmail.com>
2024-12-20 23:36:38 +1000

    Initial commit

---
+++ src/main/java/Accumulator.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Accumulator {
+    @Override
+    public String toString() {
+        return "Accumulator { capacity=12V }";
+    }
+}
